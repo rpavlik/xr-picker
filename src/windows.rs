c3d0af83c573e7c73af435cc209220db7fe07223
Ryan Pavlik <ryan.pavlik@collabora.com>
2022-11-03 10:35:09 -0500

    hey Windows stuff works again, with both bitnesses

--- src/windows.rs
+++ src/windows.rs
@@ -1,19 +1,17 @@
 // Copyright 2022, Collabora, Ltd.
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-use special_folder::SpecialFolder;
-
-use winreg::{enums::HKEY_LOCAL_MACHINE, RegKey, RegValue};
-
 use crate::{
     platform::{Platform, PlatformRuntime},
     runtime::BaseRuntime,
-    Error, OPENXR_MAJOR_VERSION,
+    Error, OPENXR, OPENXR_MAJOR_VERSION,
 };
+use special_folder::SpecialFolder;
 use std::{
     collections::{hash_map::RandomState, HashMap, HashSet},
     path::{Path, PathBuf},
 };
+use winreg::{enums::HKEY_LOCAL_MACHINE, RegKey, RegValue};
 
 #[derive(Debug, Clone)]
 pub struct WindowsRuntime {
@@ -37,24 +35,10 @@ fn system_dir_narrow() -> Option<PathBuf> {
     None
 }
 
-fn winmr_native() -> Option<PathBuf> {
-    system_dir_native().map(|d| d.join(WINMR_JSON_NAME))
-}
-
-struct BitPair<T>(T, T);
-
-#[cfg(target_arch = "x86_64")]
-fn system_dirs() -> BitPair<Option<PathBuf>> {
-    BitPair(SpecialFolder::SystemX86.get(), SpecialFolder::System.get())
-}
-#[cfg(target_arch = "x86")]
-fn system_dirs() -> BitPair<Option<PathBuf>> {
-    BitPair(SpecialFolder::System.get(), None)
-}
-
 fn make_prefix_key_native() -> PathBuf {
     Path::new("Software")
         .join("Khronos")
+        .join(OPENXR)
         .join(&OPENXR_MAJOR_VERSION.to_string())
 }
 
@@ -64,17 +48,15 @@ fn make_prefix_key_narrow() -> Option<PathBuf> {
         Path::new("Software")
             .join("WOW6432Node")
             .join("Khronos")
+            .join(OPENXR)
             .join(&OPENXR_MAJOR_VERSION.to_string()),
     )
 }
+
 #[cfg(not(target_arch = "x86_64"))]
 fn make_prefix_key_narrow() -> Option<PathBuf> {
     None
 }
-
-// fn make_available_runtimes_key() -> PathBuf {
-//         .join("AvailableRuntimes")
-// }
 
 impl WindowsRuntime {
     fn new(path: Option<&Path>, narrow_path: Option<&Path>) -> Result<Self, Error> {
@@ -104,13 +86,6 @@ impl WindowsPlatform {
 
 const AVAILABLE_RUNTIMES: &str = "AvailableRuntimes";
 
-fn make_available_runtimes_key() -> PathBuf {
-    Path::new("Software")
-        .join("Khronos")
-        .join(&OPENXR_MAJOR_VERSION.to_string())
-        .join("AvailableRuntimes")
-}
-
 fn maybe_runtime(regkey: &RegKey, kv: (String, RegValue)) -> Option<PathBuf> {
     let (val_name, _) = kv;
     let v: u32 = regkey.get_value(&val_name).ok()?;
@@ -123,7 +98,7 @@ fn maybe_runtime(regkey: &RegKey, kv: (String, RegValue)) -> Option<PathBuf> {
 fn enumerate_reg_runtimes(base_key: &Path) -> Result<Vec<PathBuf>, Error> {
     let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
     let avail = hklm
-        .open_subkey(base_key)
+        .open_subkey(base_key.to_str().unwrap())
         .map_err(|e| Error::EnumerationError(format!("Registry read error: {}", e)))?;
 
     let manifest_files = avail.enum_values().filter_map(|x| {
@@ -131,13 +106,6 @@ fn enumerate_reg_runtimes(base_key: &Path) -> Result<Vec<PathBuf>, Error> {
         maybe_runtime(&avail, x)
     });
     Ok(manifest_files.collect())
-}
-
-fn make_winmr() -> (Option<PathBuf>, Option<PathBuf>) {
-    (
-        system_dir_native().map(|d| d.join(WINMR_JSON_NAME)),
-        system_dir_narrow().map(|d| d.join(WINMR_JSON_NAME)),
-    )
 }
 
 #[derive(Default)]
@@ -190,16 +158,17 @@ impl Platform for WindowsPlatform {
         let mut collection = RuntimeCollection::default();
 
         // Manually add winmr because it will be some revisions of windows before they can put it in AvailableRuntimes
-        if let Err(_) = collection.try_add(
-            system_dir_native()
-                .map(|d| d.join(WINMR_JSON_NAME))
-                .as_ref()
-                .map(|d| d.as_path()),
-            system_dir_narrow()
-                .map(|d| d.join(WINMR_JSON_NAME))
-                .as_ref()
-                .map(|d| d.as_path()),
-        ) {
+        if collection
+            .try_add(
+                system_dir_native()
+                    .map(|d| d.join(WINMR_JSON_NAME))
+                    .as_deref(),
+                system_dir_narrow()
+                    .map(|d| d.join(WINMR_JSON_NAME))
+                    .as_deref(),
+            )
+            .is_err()
+        {
             // this is fine if it's not there
         }
 
@@ -227,27 +196,6 @@ impl Platform for WindowsPlatform {
                 );
             }
         }
-
-        // let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-        // let avail = hklm
-        //     .open_subkey(make_available_runtimes_key())
-        //     .map_err(|e| Error::EnumerationError(format!("Registry read error: {}", e)))?;
-
-        // let manifest_files = avail
-        //     .enum_values()
-        //     .filter_map(|x| {
-        //         let x = x.ok()?;
-        //         maybe_runtime(&avail, x)
-        //     })
-        //     .filter_map(|p| match WindowsRuntime::new(&p) {
-        //         Ok(r) => Some(r),
-        //         Err(e) => {
-        //             eprintln!("Error when trying to load {}: {}", p.display(), e);
-        //             None
-        //         }
-        //     })
-        //     .collect();
-
         Ok(collection.into())
     }
 }
